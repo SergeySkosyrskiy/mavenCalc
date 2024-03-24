3715c602a9618077abb7fee30a312e7b66fe9656
SergeySkosyrskiy <s-s-e_96@mail.ru>
2024-03-24 21:35:16 +0800

    Конечный вариант

--- src/main/java/pro/sky/calculator/CalController.java
+++ src/main/java/pro/sky/calculator/CalController.java
@@ -1,51 +1,66 @@
 package pro.sky.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-public class CalController  {
-   private final CalcService calcService = new CalcService();
+@RestController
+@RequestMapping()
+public class CalController {
+    private final CalcService calcService;
 
+    public CalController(CalcService calcService) {
+        this.calcService = calcService;
+    }
 
     @GetMapping(path = "/calculator")
     public String answerCalculator() {
-        return calcService.answerCalculator();
+        return "<h1>Добро пожаловать в калькулятор. <h1>";
     }
+
 
     @GetMapping(path = "/calculator/plus")
-    public String answerPlusCalculator(@RequestParam("num1") Integer num1, @RequestParam("num2") Integer num2) {
-            return calcService.answerPlusCalculator(num1,num2);
+    public String answerPlusCalculator(@RequestParam Integer num1, @RequestParam Integer num2) {
+        if (num1 == null || num2 == null) {
+            return "Ошибка!!! Введены не все данные!!!";
+        } else {
+            int result = calcService.sum(num1, num2);
+            return num1 + "+" + num2 + "=" + (num1 + num2);
+        }
+    }
+
+    @GetMapping(path = "/calculator/minus")
+    public String answerMinusCalculator(@RequestParam Integer num1, @RequestParam Integer num2) {
+        if (num1 == null || num2 == null) {
+            return "Ошибка!!! Введены не все данные!!!";
+        } else {
+            int result = calcService.subtract(num1, num2);
+            return num1 + "-" + num2 + "=" + (num1 - num2);
+        }
     }
 
 
-//    @GetMapping(path = "/calculator/minus")
-//    public String answerMinusCalculator(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-//        if (num1 == null || num2 == null || num1 == null && num2 == null) {
-//            return "Ошибка!!! Введены не все данные!!!";
-//        } else {
-//            return num1 + "-" + num2 + "=" + (num1 - num2);
-//        }
-//    }
-//
-//
-//    @GetMapping(path = "/calculator/multiply")
-//    public String answerMultiplyCalculator(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-//        if (num1 == null || num2 == null || num1 == null && num2 == null) {
-//            return "Ошибка!!! Введены не все данные!!!";
-//        } else {
-//            return num1 + "*" + num2 + "=" + (num1 * num2);
-//        }
-//    }
-//
-//
-//    @GetMapping(path = "/calculator/divide")
-//    public String answerDividelyCalculator(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-//        if (num1 == null || num2 == null || num1 == null && num2 == null) {
-//            return "Ошибка!!! Введены не все данные!!!";
-//        } else if (num2 == 0) {
-//            return "ОШИБКА!!! на 0 делить нельзя!!!";
-//        } else {
-//            return num1 + "/" + num2 + "=" + (num1 / num2);
-//        }
-//    }
+    @GetMapping(path = "/calculator/multiply")
+    public String answerMultiplyCalculator(@RequestParam Integer num1, @RequestParam Integer num2) {
+        if (num1 == null || num2 == null) {
+            return "Ошибка!!! Введены не все данные!!!";
+        } else {
+            int result = calcService.multiply(num1, num2);
+            return num1 + "*" + num2 + "=" + (num1 * num2);
+        }
+    }
+
+
+    @GetMapping(path = "/calculator/divide")
+    public String answerDividelyCalculator(@RequestParam Integer num1, @RequestParam Integer num2) {
+        if (num1 == null || num2 == null) {
+            return "Ошибка!!! Введены не все данные!!!";
+        } else if (num2 == 0) {
+            return "ОШИБКА!!! на 0 делить нельзя!!!";
+        } else {
+            int result = calcService.divide(num1, num2);
+            return num1 + "/" + num2 + "=" + (num1 / num2);
+        }
+    }
 }
--- src/main/java/pro/sky/calculator/CalcService.java
+++ src/main/java/pro/sky/calculator/CalcService.java
@@ -1,53 +1,30 @@
 package pro.sky.calculator;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
+@Service
 @RestController
-public class CalcService {
-    // int num1 = 5;
-    //  int num2 = 5;
+public class CalcService implements CalculatorService {
 
-
-    public String answerCalculator() {
-        return "<h1>Добро пожаловать в калькулятор. <h1>";
+    @Override
+    public int sum(int num1, int num2) {
+        return num1 + num2;
     }
 
-    public String answerPlusCalculator(Integer num1, Integer num2) {
-        if (num1 == null || num2 == null) {
-            return "Ошибка!!! Введены не все данные!!!";
-        } else {
-            return num1 + "+" + num2 + "=" + (num1 + num2);
-        }
+    @Override
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
     }
 
+    @Override
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
 
-//    public String answerMinusCalculator(Integer num1, Integer num2) {
-//        if (num1 == null || num2 == null || num1 == null && num2 == null) {
-//            return "Ошибка!!! Введены не все данные!!!";
-//        } else {
-//            return num1 + "-" + num2 + "=" + (num1 - num2);
-//        }
-//    }
-//
-//
-//    public String answerMultiplyCalculator(Integer num1, Integer num2) {
-//        if (num1 == null || num2 == null) {
-//            return "Ошибка!!! Введены не все данные!!!";
-//        } else {
-//            return num1 + "*" + num2 + "=" + (num1 * num2);
-//        }
-//    }
-//
-//
-//    public String answerDividelyCalculator(Integer num1, Integer num2) {
-//        if (num1 == null || num2 == null) {
-//            return "Ошибка!!! Введены не все данные!!!";
-//        } else if (num2 == 0) {
-//            return "ОШИБКА!!! на 0 делить нельзя!!!";
-//        } else {
-//            return num1 + "/" + num2 + "=" + (num1 / num2);
-//        }
-//    }
+    @Override
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
 }
